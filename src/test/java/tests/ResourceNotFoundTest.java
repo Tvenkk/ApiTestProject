7577b822cca092e448a072a46eaef8627aa9a18b
Roman Tsapko <roma.tsapko.01@mail.ru>
2025-01-09 19:18:44 +0300

    add Allure to resource tests

--- src/test/java/tests/ResourceNotFoundTest.java
+++ src/test/java/tests/ResourceNotFoundTest.java
@@ -1,17 +1,24 @@
 package tests;
 
+import io.qameta.allure.*;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
+import static io.qameta.allure.Allure.step;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@Epic("Работа с данными ресурсов")
+@Feature("Получает несуществующий ресурс")
 public class ResourceNotFoundTest {
     private final String BASE_URL = "https://reqres.in/api/unknown";
 
+    @Story("Получение несуществующего ресурса")
+    @Description("Получение ошибки 404")
+    @Severity(SeverityLevel.BLOCKER)
     @Test
     public void testResourceNotFound() {
-
+        step("Отправка GET-запроса");
         Response response = RestAssured
                 .given()
                 .when()
@@ -21,6 +28,7 @@ public class ResourceNotFoundTest {
                 .extract()
                 .response();
 
+        step("Проверка, что тело ответа пустое");
         String responseBody = response.getBody().asString();
         assertEquals("{}", responseBody, "Тело ответа не является пустым");
     }
